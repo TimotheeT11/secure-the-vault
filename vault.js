15fa313f82691c3018f2d295924891a3aee5b404
Timothee Thomance <timotheet11@gmail.com>
2025-02-18 12:56:07 -0500

    Secure the vault workshop (ready for submission)

---
+++ vault.js
@@ -0,0 +1,14 @@
+// Intro phrase
+let intro = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Code #1
+let code1 = 5 + 5;
+
+// Code #2
+let code2 = 4 * 10;
+
+// Code #3
+let code3 = 40 - 1;
+
+// Display alert with full code
+alert(`${intro} ${code1} - ${code2} - ${code3}`);
